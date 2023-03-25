28325742e804e51f27442d720faa7a41d0dfaacb
Sara Gisondi <sara.gisondi@outlook.it>
2023-03-25 18:19:28 +0100

    add price

--- js/script.js
+++ js/script.js
@@ -1,60 +1,56 @@
-// //CHIEDERE ALL'UTENTE I KM CHE VUOLE PERCORRERE E LA SUA ETA'
-// const km = ('inserisci i km che vuoi percorrere')
-// const age = ('inserisci la tua età')
-// console.log('km', 'age')
-// const name = ('inserisci il tuo nome');
-// console.log('name')
-// //KM
-// document.getElementById('km').innerHTML = (km);
-// console.log(km)
+//CHIEDERE ALL'UTENTE I KM CHE VUOLE PERCORRERE E LA SUA ETA'
+const km = 300;
+const age = 66;
+console.log(km, age)
+const name = 'sara';
+console.log(name)
+//KM
+document.getElementById('km').innerHTML = (km);
+console.log(km)
 
 // //ETA'
 // document.getElementById('age').innerHTML = (age);
 // console.log(age)
 
-// //PREZZO TOTALE DEL VIAGGIO
-// const price = 0.21;
-// console.log(price);
-// const totalPrice = (price * km);
+//PREZZO TOTALE DEL VIAGGIO
+const price = 0.21;
+console.log(price);
+const totalPrice = (price * km);
+console.log(totalPrice)
 
 // //SCONTISTICA
 
-// //UNDER18
-// const discount20 =  (20/100) * totalPrice;
-// console.log(discount20)
+//UNDER18
+const discount20 =  (20/100) * totalPrice;
+console.log(discount20)
 
-// const under18 = totalPrice - discount20;
-// console.log(under18)
-
-
-// //OVER65
-// const discount40 = (40/100) * totalPrice;
-// console.log(discount40)
-
-// const over65 = totalPrice - discount40;
-// console.log(over65)
+const under18 = totalPrice - discount20;
+console.log(under18)
 
 
-// let ticket="";
-// //CONDIZIONE
-// if(age < 18){
-//   ticket = under18;
-//   document.getElementById('young').innerHTML = `
-//   Ciao ${name}, <br> hai diritto a uno sconto del 20%! <br> Spenderai ${under18} anziché ${totalPrice}
-//   `
-// }else if ( age > 18 && age <65){
-//   ticket = totalPrice;
-//   document.getElementById('age-range').innerHTML = `
-//   Ciao ${name}, il prezzo del tuo biglietto è di  ${totalPrice}.
-//   `
-// }
+//OVER65
+const discount40 = (40/100) * totalPrice;
+console.log(discount40)
 
-// if(age > 65){
-//   ticket = over65;
-//   document.getElementById('senior').innerHTML = `
-//   Ciao ${name}, <br> hai diritto a uno sconto del 40%! <br> Spenderai ${over65} anziché ${totalPrice}
-//   `
-// }
+const over65 = totalPrice - discount40;
+console.log(over65)
+
+
+let ticket="";
+//CONDIZIONE
+if(age < 18){
+  ticket = under18;
+  document.getElementById('price-ticket').innerHTML = (under18 + '€');
+
+}else if ( age > 18 && age <65){
+  ticket = totalPrice;
+  document.getElementById('price-ticket').innerHTML = (totalPrice + '€');
+}
+
+if(age > 65){
+  ticket = over65;
+  document.getElementById('price-ticket').innerHTML = (over65 + '€')
+}
 
 
 
