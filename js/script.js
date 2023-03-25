817c028d6fb04c5febc02dc7ab3f8f9f77df5696
Sara Gisondi <sara.gisondi@outlook.it>
2023-03-25 22:42:52 +0100

    add carriage number

--- js/script.js
+++ js/script.js
@@ -17,11 +17,66 @@ document.getElementById("genera").addEventListener('click', function (){
   document.getElementById("offerta").innerHTML = (cartaFreccia);
   console.log(cartaFreccia)
 
+  //PREZZO TOTALE DEL VIAGGIO
+const price = 0.21;
+console.log(price);
+const totalPrice = (price * km);
+console.log(totalPrice)
 
- 
+//SCONTISTICA
+
+//UNDER18
+const discount20 =  (20/100) * totalPrice;
+console.log(discount20)
+
+const under18 = totalPrice - discount20;
+console.log(under18)
+
+
+//OVER65
+const discount40 = (40/100) * totalPrice;
+console.log(discount40)
+
+const over65 = totalPrice - discount40;
+console.log(over65)
+
+let ticket="";
+//CONDIZIONE
+if ( cartaFreccia == "Senior"){
+  ticket = over65;
+  document.getElementById('price-ticket').innerHTML = `
+  ${over65}€
+  `
+  document.getElementById('cart').innerHTML = `
+  ${totalPrice}€
+  `
+}else if(cartaFreccia !== "Senior"){
+  ticket = under18;
+  document.getElementById('price-ticket').innerHTML = `
+  ${under18}€
+  `
+  document.getElementById('cart').innerHTML = `
+  ${totalPrice}€
+  `
+}
+
+
+if(cartaFreccia == "Nessuna"){
+  ticket = totalPrice;
+  document.getElementById('price-ticket').innerHTML = `
+  ${totalPrice}€
+  `
+  document.getElementById('cart').innerHTML = `
+  ${totalPrice}€
+  `
+}
+//NUMERO CARROZZA
+const carriageRandom = Math.floor(Math.random()*12);
+document.getElementById("train-carriage").innerHTML = `
+${carriageRandom}
+`
+console.log(carriageRandom)
 
 })
-
-
 
 
